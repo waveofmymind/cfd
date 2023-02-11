746784b085b96066c0e3379fcf6a813e36e47b36
waveofmymind <93868431+waveofmymind@users.noreply.github.com>
2023-02-12 00:05:08 +0900

    Delete shop directory

--- shop/src/main/java/wave/shop/domain/cart/repository/CartRepository.java
+++
@@ -1,6 +0,0 @@
-package wave.shop.domain.cart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wave.shop.domain.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer> {}
--- shop/src/main/java/wave/shop/domain/item/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package wave.shop.domain.item.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wave.shop.domain.item.domain.Item;
-
-
-public interface ItemRepository extends JpaRepository<Item,Long> {
-}
